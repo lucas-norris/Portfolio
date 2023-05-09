574b714404cb259edc4841a83b0dfbcfbab875ae
Lucas Norris <luke.norris22@gmail.com>
2023-05-09 16:57:23 +1200

    nav bar on scroll working and simplified styling

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Hide Header on on scroll down
-var didScroll
-var lastScrollTop = 0
-var delta = 5
-var navbarHeight = $('header').outerHeight()
+let didScroll
+let lastScrollTop = 0
+let delta = 5
+let navbarHeight = $('nav').outerHeight()
 
 $(window).scroll(function (event) {
   didScroll = true
@@ -16,7 +16,7 @@ setInterval(function () {
 }, 250)
 
 function hasScrolled() {
-  var st = $(this).scrollTop()
+  let st = $(this).scrollTop()
 
   // Make sure they scroll more than delta
   if (Math.abs(lastScrollTop - st) <= delta) return
@@ -25,11 +25,11 @@ function hasScrolled() {
   // This is necessary so you never see what is "behind" the navbar.
   if (st > lastScrollTop && st > navbarHeight) {
     // Scroll Down
-    $('header').removeClass('nav-down').addClass('nav-up')
+    $('nav').removeClass('nav-down').addClass('nav-up')
   } else {
     // Scroll Up
     if (st + $(window).height() < $(document).height()) {
-      $('header').removeClass('nav-up').addClass('nav-down')
+      $('nav').removeClass('nav-up').addClass('nav-down')
     }
   }
 
