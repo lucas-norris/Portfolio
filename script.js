e24639a4d2f23e6365ae546c86f5c33b48847678
Lucas Norris <luke.norris22@gmail.com>
2023-05-28 14:34:25 +1200

    added responsive styling and functionality with hamburger menu

--- script.js
+++ script.js
@@ -25,13 +25,71 @@ function hasScrolled() {
   // This is necessary so you never see what is "behind" the navbar.
   if (st > lastScrollTop && st > navbarHeight) {
     // Scroll Down
-    $('nav').removeClass('nav-down').addClass('nav-up')
+    $('#top-nav').removeClass('nav-down').addClass('nav-up')
   } else {
     // Scroll Up
     if (st + $(window).height() < $(document).height()) {
-      $('nav').removeClass('nav-up').addClass('nav-down')
+      $('#top-nav').removeClass('nav-up').addClass('nav-down')
     }
   }
 
   lastScrollTop = st
+}
+
+// Get references to the button and aside element
+const button = document.querySelector('.hamburger')
+const aside = document.querySelector('.menu-sidebar')
+const hamBoxInner = document.querySelector('.ham-box-inner')
+const menuLinks = document.querySelectorAll('.menu-sidebar a') // Get all the <a> elements inside the menu-sidebar
+const body = document.querySelector('body')
+const mainSection = document.querySelector('main')
+
+// Add click event listener to the button
+button.addEventListener('click', function () {
+  toggleMenu()
+})
+
+// Add click event listener to each menu link
+menuLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    toggleMenu()
+  })
+})
+
+// Function to toggle the menu visibility
+function toggleMenu() {
+  // Toggle the aria-hidden attribute of the aside element
+  const isHidden = aside.getAttribute('aria-hidden') === 'true'
+  aside.setAttribute('aria-hidden', !isHidden)
+  aside.classList.toggle('menu-sidebar-visible')
+  hamBoxInner.classList.toggle('ham-box-inner-x')
+
+  // Change the tabindex attribute of the aside element
+  const tabindexValue = isHidden ? '1' : '-1'
+  aside.setAttribute('tabindex', tabindexValue)
+
+  //Toggle scroll disable on body element
+  body.classList.toggle('menu-open')
+
+  //Toggle blur effect on main section
+  mainSection.classList.toggle('blur')
+
+  //Disable scroll on mobile devices
+  if (isMobileDevice()) {
+    toggleMobileScroll(!isHidden)
+  }
+}
+
+// Function to check if the user is on a mobile device
+function isMobileDevice(disableScroll) {
+  if (disableScroll) {
+    body.addEventListener('touchmove', preventDefault, { passive: false })
+  } else {
+    body.removeEventListener('touchmove', preventDefault)
+  }
+}
+
+// Function to prevent default touchmove event
+function preventDefault(e) {
+  e.preventDefault()
 }
